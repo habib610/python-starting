7ebce0a55dbe11d4a607ed09b383cf7c6283af96
habib610 <habib54562@gmail.com>
2021-01-27 00:33:12 +0600

    power-math

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello world", 5+5, end="")
+import math;
+
+firstOne = 3;
+powerValue = firstOne ** firstOne
+
+print(powerValue)
+
+second = 4
+secondValue = math.pow(second, 3)
+print(secondValue)
