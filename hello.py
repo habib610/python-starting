6f0d37d0cf55b0ab66a31cac3c00dcf11556004d
habib610 <habib54562@gmail.com>
2021-01-27 22:56:44 +0600

    slice-method

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
-""" helal adworld
-akdfkadfkasdf
-asdkfakdsf
-asdfasdf"""
+#slice method
+#NOTE: starter is always inclusive and starter is always exclusive
 
-"""
-print("hello world")
-"""
-poem = """ helal adworld
-akdfkadfkasdf
-asdkfakdsf
-asdfasdf"""
-print(poem)
+one = "0123456789"
+print(one[3:]) #inclusive (included the index); output = 3456789
+
+two = "0123456789"
+print(two[:4]) #exclusive (doesn't included the index); output = 0123
+
+three = "0123456789"
+print(three[3:7])  #included the starter index and excluded outer index; output = 3456
+
+four = "0123456789"
+print(four[:-1]) #started from the last index; output = 012345678
