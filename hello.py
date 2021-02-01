41bbcfd84793369acb2d7474228551668a1a3975
habib610 <habib54562@gmail.com>
2021-02-01 16:13:56 +0600

    copying-objects

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
-#string length len and str method
+my_fav_things = ["workingout", 356, 45, ["reading", 45]]
+print(my_fav_things)
 
-name = "habibur rahman"
-print(len(name))
+copy = my_fav_things #we are not copying; we are making a new alias
+print(copy)
 
-print("your message" + str(len(name)) + "characters") #you cannot concate  string and number
-# to concate string and number use str methos which 
+copy[0] = "hangout"
+print(my_fav_things)
+print(copy)
+
+#to copy a list we can do like this
+
+my_second_fav = ["programming", 344, 566, 67, ["sleeping"]]
+
+second_copy = my_fav_things[:] #By Slicing
+
+#or By using copy() function
+# second_copy = my_second_fav.copy()
+
+second_copy[0] = "Development"
+
+
+print(my_second_fav, second_copy)
