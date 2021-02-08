516ac752044ccb672a5d16646ceec31dd8b01a24
habib610 <habib54562@gmail.com>
2021-02-08 11:46:31 +0600

    logic-and-indent

--- hello.py
+++ hello.py
@@ -1,30 +1,11 @@
-#Taking user input
-#Parsing data
+#logic and indentation
 
-# print(type(5))
-# print(type("5"))
+print("Whats your age?")
+age = int(input())
 
-# print(type(int("5")))
-# print(type(int("5GAKFKASF"))) this wont worked
+if age > 50: 
+    print("you are a big guy")
 
+    print("This line is not  executing now!!!")
 
-# print("whata's your name?")
-# name = input()
-# print("what's your favorite number?")
-# num1 = input()
-# print("another favorite number?")
-# num2 = input()
-# print("Hey!", name, "your favorite sum is", num1 + num2)
-
-
-
-
-
-
-print("what's your name?")
-name = input()
-print("what's your favorite number?")
-num1 = int(input())
-print("another favorite number?")
-num2 = int(input())
-print("Hey!" + str(name) + "your favorite sum is "+ str(num1 + num2))
+print("This line is executing now!!")
