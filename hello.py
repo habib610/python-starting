88fdae2817222ab8152cf2e44330774cd194b2c5
habib610 <habib54562@gmail.com>
2021-02-08 10:59:46 +0600

    type-conversion

--- hello.py
+++ hello.py
@@ -1,10 +1,30 @@
 #Taking user input
+#Parsing data
 
-print("whata's your name?")
+# print(type(5))
+# print(type("5"))
+
+# print(type(int("5")))
+# print(type(int("5GAKFKASF"))) this wont worked
+
+
+# print("whata's your name?")
+# name = input()
+# print("what's your favorite number?")
+# num1 = input()
+# print("another favorite number?")
+# num2 = input()
+# print("Hey!", name, "your favorite sum is", num1 + num2)
+
+
+
+
+
+
+print("what's your name?")
 name = input()
 print("what's your favorite number?")
-num1 = input()
+num1 = int(input())
 print("another favorite number?")
-num2 = input()
-
-print("Hey!", name, "your favorite sum is", num1 + num2)
+num2 = int(input())
+print("Hey!" + str(name) + "your favorite sum is "+ str(num1 + num2))
